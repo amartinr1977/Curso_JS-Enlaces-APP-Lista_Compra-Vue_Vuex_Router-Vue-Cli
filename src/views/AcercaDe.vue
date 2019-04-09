2fefc4a23c121cceab4b053bc728f70d5875bece
amartinr1977 <amartinromero@gmail.com>
2019-04-09 21:01:30 +0200

    En LocalStorage se almacena la lista de la compra

--- src/views/AcercaDe.vue
+++ src/views/AcercaDe.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>Esto es Acerca de ...</div>
+  <div class="col-12">
+    <div class="row cabecera justify-content-center mt-5" id="cabecera">
+      <h1 id="titulo1">
+        <i class="material-icons" style="font-size: 1em">shopping_cart</i>
+        Información Básica de Uso de la App
+      </h1>
+    </div>
+  </div>
 </template>
